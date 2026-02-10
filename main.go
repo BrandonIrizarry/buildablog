8336150dbf80b0d7d8a8494fbd76651492c4f225
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-02-10 14:48:42 -0500

    feat: update handler to dump a file to the browser
    The URL and filepath closely coincide. For example the URL
    
    http://localhost:3030/posts/apple
    
    maps to the local file
    
    posts/apple.md.

--- main.go
+++ main.go
@@ -2,19 +2,64 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	// Set up logging.
+	logFile, err := os.OpenFile("buildablog.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Printf("can't open logfile: %v", err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
 
-	mux.HandleFunc("GET /posts/{slug}", func(w http.ResponseWriter, r *http.Request) {
-		slug := r.PathValue("slug")
-		fmt.Fprintf(w, "Post: %s", slug)
-	})
+	// Set up the server.
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /posts/{slug}", postHandler(markdownFileHandler))
 
 	if err := http.ListenAndServe(":3030", mux); err != nil {
 		log.Fatal(err)
+	}
+}
+
+// markdownFileHandler returns the blog text found at the given slug
+// path.
+func markdownFileHandler(slug string) (string, error) {
+	filename := fmt.Sprintf("posts/%s.md", slug)
+	log.Printf("Will attempt to open %s", filename)
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	bytes, err := io.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+// postHandler "decorates" the given reader using an
+// [http.HandlerFunc].
+func postHandler(reader reader) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Slug: %s", r.PathValue("slug"))
+
+		slug := r.PathValue("slug")
+		blogText, err := reader(slug)
+		if err != nil {
+			http.Error(w, "Post not found", http.StatusNotFound)
+			return
+		}
+
+		fmt.Fprint(w, blogText)
 	}
 }
---
+++ types.go
@@ -0,0 +1,3 @@
+package main
+
+type reader func(string) (string, error)
