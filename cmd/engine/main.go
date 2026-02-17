a6f6b7c5781ecd74f15e4d3ce20543469d54cb9e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-02-17 15:28:09 -0500

    refactor: remove unused -publish flag, along with its logic
    It has now served its purpose, since we're onto using -candidates
    with a Makefile.

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -13,7 +13,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/BrandonIrizarry/buildablog/internal/constants"
 	"github.com/BrandonIrizarry/buildablog/internal/readers"
 	"github.com/BrandonIrizarry/buildablog/internal/types"
 )
@@ -47,18 +46,10 @@ func (cl *candidatesList) Set(value string) error {
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	var (
-		candidate  string
-		candidates candidatesList
-	)
+	var candidates candidatesList
 
-	flag.StringVar(&candidate, "publish", "", "Publish this post")
 	flag.Var(&candidates, "candidates", "List of blog posts to update")
 	flag.Parse()
-
-	if candidate != "" {
-		publish(candidate)
-	}
 
 	if len(candidates) > 0 {
 		if err := updateCandidates(candidates); err != nil {
@@ -202,62 +193,4 @@ func updateCandidates(candidates candidatesList) error {
 	}
 
 	return nil
-}
-
-// publish publishes the given candidate.
-//
-// This marshals the title and description into a file, which the
-// /archives endpoint reads and uses to define its content.
-func publish(candidate string) {
-	// We want to take advantage of terminal auto-completion,
-	// since blog slugs are often long and bespoke, often closely
-	// mirroring their actual titles. Because of this, candidate
-	// is read in as a relative path starting from
-	// [constants.ContentDirName]. But now we need to do extra
-	// work to split this apart.
-	dir, file := filepath.Split(candidate)
-	if !strings.HasPrefix(dir, constants.ContentDirName) {
-		log.Fatalf("'%s' isn't inside %s", dir, constants.ContentDirName)
-	}
-
-	label := strings.TrimPrefix(dir, constants.ContentDirName)
-	slug := strings.TrimSuffix(file, ".md")
-
-	data, err := readers.ReadPage(slug, label)
-	if err != nil {
-		log.Fatalf("couldn't read content/posts/%s: %v", candidate, err)
-	}
-
-	if !data.Publish {
-		// Either the file was never published, or else
-		// recently demoted from "published" status.
-		log.Printf("'%s' is a draft, and so will not be published", candidate)
-		os.Exit(0)
-	}
-
-	b, err := json.Marshal(types.PublishData{
-		Date:    time.Now().Format(time.DateOnly),
-		Slug:    slug,
-		Title:   data.Title,
-		Summary: data.Summary,
-	})
-	if err != nil {
-		log.Fatalf("couldn't marshal title and summary: %v", err)
-	}
-
-	f, err := os.OpenFile("published", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("couldn't open 'published' file: %v", err)
-	}
-	defer f.Close()
-
-	// Append a newline for human-readability purposes, as well as
-	// for the 'GET /archives' endpoint to be able to read this
-	// file line by line.
-	b = append(b, '\n')
-
-	_, err = f.Write(b)
-	if err != nil {
-		log.Fatalf("couldn't write to 'published' file: %v", err)
-	}
 }
