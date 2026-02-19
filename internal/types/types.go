93ef4d192d9157570b44ddf45ed5181a89262a51
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-02-19 17:49:25 -0500

    docs: add comprehensive comments to types.go

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,18 +1,36 @@
 package types
 
-// frontmatter fields
+// FrontmatterData is used for unmarshalling blog post frontmatter.
 type FrontmatterData struct {
-	Title   string   `toml:"title"`
-	Summary string   `toml:"summary"`
-	Tags    []string `toml:"tags"`
-	Publish bool     `toml:"publish"`
+	// Title is the blog post's title.
+	Title string `toml:"title"`
+
+	// Summary is a short summary of the blog post.
+	Summary string `toml:"summary"`
+
+	// Tags is the list of tags categorizing the blog post.
+	Tags []string `toml:"tags"`
+
+	// Publish flags whether to publish the blog post into the
+	// publishing file.
+	Publish bool `toml:"publish"`
 }
 
-// inside published.json
+// PublishData is used from marshalling/unmarshalling the currently
+// published archives.
+//
+// Note that some fields are repeated from [FrontmatterData]; this is
+// done to avoid obfuscating the concerns of a given struct type
+// solely for the sake of DRY.
 type PublishData struct {
-	Title   string   `json:"title"`
-	Summary string   `json:"summary"`
-	Tags    []string `json:"tags"`
+	// Title is the blog post's title.
+	Title string `json:"title"`
+
+	// Summary is a short summary of the blog post.
+	Summary string `json:"summary"`
+
+	// Tags is the list of tags categorizing the blog post.
+	Tags []string `json:"tags"`
 
 	// Created details when the post was created (Unix timestamp.)
 	Created int64 `json:"created"`
