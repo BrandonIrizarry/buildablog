399dca97b6b23d54cf3935f3c6e909277752cd15
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-02-24 17:17:49 -0500

    feat: restore the RSS feed endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"html/template"
@@ -9,10 +10,12 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strings"
 	"time"
 
 	"github.com/BrandonIrizarry/buildablog/internal/constants"
 	"github.com/BrandonIrizarry/buildablog/internal/readers"
+	"github.com/BrandonIrizarry/buildablog/internal/rss"
 	"github.com/BrandonIrizarry/buildablog/internal/types"
 )
 
@@ -164,6 +167,81 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+	})
+
+	// Serve the RSS feed.
+	mux.HandleFunc("GET /rss", func(w http.ResponseWriter, r *http.Request) {
+		siteTitle := "Biome of Ideas"
+		var siteURL string
+
+		// Use flagLocal to set the correct siteURL for
+		// purposes of testing the RSS feed locally with
+		// something like newsboat.
+		if flagLocal {
+			siteURL = "http://localhost:3030"
+		} else {
+			siteURL = "https://brandonirizarry.xyz"
+		}
+
+		posts, err := readers.AllPosts()
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var items []rss.Item
+		for _, post := range posts {
+			link := fmt.Sprintf("%s/%s/%s", siteURL, constants.PostsLabel, post.Date.Format(time.DateOnly))
+			pubDate := post.Date.Format(time.RFC1123)
+
+			item := rss.Item{
+				Title:   post.Title,
+				Link:    link,
+				GUID:    link,
+				PubDate: pubDate,
+				Description: rss.Description{
+					Type: "html",
+					Text: post.Content,
+				},
+			}
+
+			items = append(items, item)
+		}
+
+		image := rss.Image{
+			Title:  siteTitle,
+			Link:   siteURL,
+			URL:    fmt.Sprintf("%s/static/bitmap.png", siteURL),
+			Width:  80,
+			Height: 80,
+		}
+
+		rssChannel := rss.Channel{
+			Title:       siteTitle,
+			Link:        siteURL,
+			Description: "My personal website and blog",
+			Language:    "en-us",
+			Image:       image,
+			Items:       items,
+		}
+
+		mainRSS := rss.RSS{
+			Channel: rssChannel,
+			Version: "2.0",
+		}
+
+		// Marshal the data to XML
+		feed, err := xml.MarshalIndent(mainRSS, "", strings.Repeat(" ", 4))
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprint(w, xml.Header+string(feed))
 	})
 
 	// Serve the site's static assets (CSS files etc.)
--- internal/rss/types.go
+++ internal/rss/types.go
@@ -1,6 +1,9 @@
 package rss
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"html/template"
+)
 
 // Channel is used for marshalling data into the blog's RSS feed.
 type Channel struct {
@@ -12,13 +15,18 @@ type Channel struct {
 	Items       []Item `xml:"item"`
 }
 
+type Description struct {
+	Type string        `xml:"type,attr"`
+	Text template.HTML `xml:",chardata"`
+}
+
 // Item is used to enumerate a blog post's mention in the RSS feed.
 type Item struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	GUID        string `xml:"guid"`
-	PubDate     string `xml:"pubDate"`
-	Description string `xml:"description"`
+	Title       string      `xml:"title"`
+	Link        string      `xml:"link"`
+	GUID        string      `xml:"guid"`
+	PubDate     string      `xml:"pubDate"`
+	Description Description `xml:"description"`
 }
 
 // Image is used to display an image when aggregators present a field.
