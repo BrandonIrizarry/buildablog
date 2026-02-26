daa0f1f9d56aebb5333e4e452e4355ece3cb9d23
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-02-26 15:57:37 -0500

    feat: introduce internal/projects package with doc.go

---
+++ internal/projects/doc.go
@@ -0,0 +1,8 @@
+// Package projects defines struct types, constants, and helper
+// functions needed for our Projects Gallery.
+//
+// The Project Gallery scheme is almost exactly like that used for
+// blogs: there is a listings page (the Project Gallery) detailing all
+// entries in summary form, as well as pages dedicated to each
+// individual entry.
+package projects
