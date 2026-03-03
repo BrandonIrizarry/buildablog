46ce4b16f2807d1c2eaa32eb0618223a55be6b3b
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-03-02 19:01:46 -0500

    feat: begin injecting project data into Projects page

--- cmd/server/getProjects.go
+++ cmd/server/getProjects.go
@@ -3,10 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/BrandonIrizarry/buildablog/internal/projects"
+	"github.com/BrandonIrizarry/buildablog/internal/readers"
 )
 
 func getProjects(w http.ResponseWriter, r *http.Request) {
-	if err := feedTemplate(w, "projects", struct{}{}); err != nil {
+	ps, err := readers.AllArticles[projects.Frontmatter](nil)
+	if err != nil {
+		log.Printf("%v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := feedTemplate(w, "projects", ps); err != nil {
 		log.Printf("%v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
